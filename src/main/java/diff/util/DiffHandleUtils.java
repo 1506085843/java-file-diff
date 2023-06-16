f225351f2d84da895a5a92dd7a2a0f9ae8e27c20
xiaohaitang <39946319+1506085843@users.noreply.github.com>
2023-06-16 13:34:26 +0800

    Update DiffHandleUtils.java
    修改第一行文本改变导致的问题

--- src/main/java/diff/util/DiffHandleUtils.java
+++ src/main/java/diff/util/DiffHandleUtils.java
@@ -205,11 +205,11 @@ public class DiffHandleUtils {
                 insert(result, getOrigList(original, start, end));
             }
 
+            int start = (map.get("orgRow") + map.get("orgDel") - 1);
+            start = start == -1 ? 0 : start;
             if (simb.contains("@@ -1,") && null == nexSimb && map.get("orgDel") != original.size()) {
-                insert(result, getOrigList(original, 1, original.size() - 1));
+                insert(result, getOrigList(original, start, original.size() - 1));
             } else if (null == nexSimb && (map.get("orgRow") + map.get("orgDel") - 1) < original.size()) {
-                int start = (map.get("orgRow") + map.get("orgDel") - 1);
-                start = start == -1 ? 0 : start;
                 insert(result, getOrigList(original, start, original.size() - 1));
             }
         }
